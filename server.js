37fd074879a1522186bd0180c114005d9ef53556
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-05-19 22:42:36 -0600

    began work on helper functions that will collect data and save it for notes later

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require("path");
+const fs = require("fs");
+const util = require("util")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,19 +9,62 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-notes = []
-
-app.get('/', (req, res) => 
-  res.sendFile(path.join(__dirname, "public/index.html"))
+const getDB = () =>
+  JSON.parse(
+    fs.readFileSync(path.join(__dirname, "db", "db.json"), {
+      encoding: "utf-8",
+    })
 );
 
+const saveDB = (data) =>
+  fs.writeFileSync(
+    path.join(__dirname, "db", "db.json"),
+    JSON.stringify(data),
+    { encoding: "utf-8" }
+);
+
+notes = []
+
 app.get("/notes", (req, res) => 
-  res.sendFile(path.join(__dirname, "public/notes.html"))
+res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 app.post('/notes', (req, res) => {
-    const notes = req.body;
-    res.json(notes);
+  const notes = req.body;
+  res.json(notes);
+});
+
+//nicks notes
+
+// const readFileAsync = util.promisify(fs.readfile);
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+// const getNotes =  () =>{
+//   return fs.readFileAsync("db/db.json", "utf8").then((notes) => {
+//   return JSON.parse(notes);
+//   });
+// };
+
+// app.get("/api.notes", (req, res) => {
+//   getNotes().then((notes) => {
+//     res.JSON(notes);
+//   });
+// });
+
+// app.post("api/notes",(req, res) => {
+//   const allNotes = notes;
+//   allNotes.push(newNote);
+//   return writeFileAsync("db/db.json", JSON.stringify(allNotes)).then(() =>{
+//     res.json(newNote);
+//   });
+// });
+
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+console.log(req.url);
+  res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
