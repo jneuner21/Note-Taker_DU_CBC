9c3aeb81638b76f17b0714e1ba3fe980a7637eb3
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-05-20 22:53:10 -0600

    double checking commit test for heroku

--- server.js
+++ server.js
@@ -1,70 +1,51 @@
 const express = require('express');
 const path = require("path");
 const fs = require("fs");
-const util = require("util")
+const dbObj = require ("./db/db.json");
+const { send } = require('process');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const getDB = () =>
-  JSON.parse(
-    fs.readFileSync(path.join(__dirname, "db", "db.json"), {
-      encoding: "utf-8",
-    })
-);
-
-const saveDB = (data) =>
-  fs.writeFileSync(
-    path.join(__dirname, "db", "db.json"),
-    JSON.stringify(data),
-    { encoding: "utf-8" }
-);
-
-notes = []
-
-app.get("/notes", (req, res) => 
-res.sendFile(path.join(__dirname, "public/notes.html"))
-);
-
-app.post('/notes', (req, res) => {
-  const notes = req.body;
-  res.json(notes);
-});
-
-//nicks notes
-
-// const readFileAsync = util.promisify(fs.readfile);
-// const writeFileAsync = util.promisify(fs.writeFile);
-
-// const getNotes =  () =>{
-//   return fs.readFileAsync("db/db.json", "utf8").then((notes) => {
-//   return JSON.parse(notes);
-//   });
-// };
-
-// app.get("/api.notes", (req, res) => {
-//   getNotes().then((notes) => {
-//     res.JSON(notes);
-//   });
-// });
-
-// app.post("api/notes",(req, res) => {
-//   const allNotes = notes;
-//   allNotes.push(newNote);
-//   return writeFileAsync("db/db.json", JSON.stringify(allNotes)).then(() =>{
-//     res.json(newNote);
-//   });
-// });
-
-
+//routes
 app.use(express.static("public"));
-
 app.get('/', (req, res) => {
 console.log(req.url);
-  res.sendFile(path.join(__dirname, "public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get("/notes", (req, res) => 
+res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get('/api/notes', (req, res) => {
+res.json(dbObj);
+});
+
+app.post('/api/notes', (req, res) => {
+  const dbObj = require("./db/db.json")
+  const newNote = req.body;
+  newNote.id = dbObj.length > 0 ? dbObj[dbObj.length - 1].id + 1 : 1;
+  dbObj.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(dbObj)
+  );
+  res.send("newNote");
+});
+
+
+//
+app.delete('/api/notes/:id', (req, res) => {
+  const idNum = req.params.id;
+  console.log(idNum);
+  const dbJson = require("./db/db.json");
+  const newObj = dbJson.filter((item) => item.id != idNum);
+  fs.writeFileSync("./db/db.json", JSON.stringify(newObj));
+  newObj = JSON.parse(newObj);
+  // res.send(newObj);
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
